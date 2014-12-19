84e5bdce4de501f15b6ad173c1c79f8085f386fc
SmokeytheSalmon <Roelvanbakkum@gmail.com>
2014-12-19 13:31:19 +0100

    Make ding.js slightly more dynamic in generating the widget

--- ding.js
+++ ding.js
@@ -1,2 +1,29 @@
-var div = document.getElementById('widget');
-div.innerHTML = div.innerHTML + 'Hallotjes';
+var widget = document.getElementById('widget');
+var leftMenu = document.createElement('ul');
+leftMenu.setAttribute("class", "left-menu");
+
+function addItem(content, tag){
+  var item = document.createElement('li');
+  var anchor = document.createElement('a');
+  if(tag) {
+    anchor.setAttribute("class", tag);
+  }
+  anchor.innerHTML += content;
+  item.appendChild(anchor);
+  leftMenu.appendChild(item);
+
+/* Left For Debugging Purposes
+  console.log(content);
+  console.log(item);
+  console.log(anchor);
+  console.log(leftMenu);*/
+}
+
+var elements = ["title", "description", "link"];
+
+for (element in elements) {
+  addItem(elements[element], elements[element]);
+}
+
+widget.appendChild(leftMenu);
+console.log(widget);
