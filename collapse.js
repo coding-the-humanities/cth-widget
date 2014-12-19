add19d02a33880bf6850e0dd9ba52989e4624f4f
evanzummeren <erikvanzummeren@gmail.com>
2014-12-19 13:52:15 +0100

    widget m

--- collapse.js
+++ collapse.js
@@ -8,3 +8,24 @@ $(function() {
     // e.stopPropagation();
   });
 });
+
+// var el = document.querySelector("ul.left-menu li > ul");
+// var elcomplete = document.querySelector("ul.left-menu li");
+
+// console.log("code is run");
+// console.log(el);
+// console.log(elcomplete);
+
+
+// elcomplete.addEventListener("click", function(e){
+// 	console.log("Event is triggered");
+	
+// 	if (el.style.display == "none") {
+// 		console.log("display= hidden");
+// 		el.style.display = "initial";
+// 	} else {
+// 		console.log("display= something else");
+// 		el.style.display = "none";
+// 	}
+// 	// e.stopPropagation();
+// });
